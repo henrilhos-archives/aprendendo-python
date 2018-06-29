c7ae5c353ed9c709bc5ce07e114020788bb7e38d
castilh0s <henriquedecastilhos@gmail.com>
2018-06-29 20:47:06 -0300

    Resolvido

--- questions/question_09.py
+++ questions/question_09.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# 9. Faça um algoritmo que calcule e apresente o valor do volume de uma lata de
+# óleo, dado seu raio e sua altura.
+
+pi = 3.14
+
+raio = input("Informe o raio da lata de óleo: ")
+altura = input("Informe a altura da lata de óleo: ")
+
+volume = pi * (float(raio) ** 2) * float(altura)
+
+print("\nO volume da lata de óleo é de: " + str(volume))
--- questions/question_10.py
+++ questions/question_10.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# 10. Converter um inteiro informado menor que 32 para sua representação em binário
+
+num = input("Informe um número inteiro menor que 32: ")
+cont = 0
+
+if int(num) <= 32:
+    bin = bin(int(num))
+    print("\nValor em binário: " + str(bin))
+else:
+    print("\nNúmero inválido.")
