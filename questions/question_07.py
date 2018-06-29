e08c1c02fd596e2d94e0623194e7ebe7df26e3e3
castilh0s <henriquedecastilhos@gmail.com>
2018-06-29 20:12:47 -0300

    Ex. 07 resolvido

--- questions/question_07.py
+++ questions/question_07.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# 7. Solicitar a idade de uma pessoa em dias e informar na tela a idade em anos, meses e dias.
+
+anos = 0
+meses = 0
+cont = 1
+
+dias = input("Informe a idade em dias: ")
+
+while int(cont) <= int(dias):
+    if int(dias) >= 365:
+        anos = int(anos) + 1
+        dias = int(dias) - 365
+    if int(dias) >= 30:
+        meses = int(meses) + 1
+        dias = int(dias) - 30
+        if int(meses) == 12:
+            anos = int(anos) + 1
+            meses = int(meses) - 12
+    cont = int(cont) + 1
+
+print("\nA pessoa tem " + str(anos) + " ano(s), " + str(meses) + " mes(es) e " + str(dias) + " dia(s)")
