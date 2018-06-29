04f63f4bee11c157b48120ca1f270d0f164869de
castilh0s <henriquedecastilhos@gmail.com>
2018-06-29 20:19:45 -0300

    Ex. 08 resolvido

--- questions/question_08.py
+++ questions/question_08.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# 8. Dado que a fórmula para conversão de Fahrenheit para Celsius é C = 5/9 (F – 32),
+# ler um valor de temperatura em Fahrenheit e exibi-lo em Celsius
+
+fahrenheit = input("Informe a temperatura em Fahrenheit: ")
+
+celsius = (5 / 9) * (float(fahrenheit) - 32)
+
+print("\nTemperatura em Celsius: " + str(celsius))
