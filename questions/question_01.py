0c940a2601f31a43325fa476f97a10728b35374a
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2018-06-29 12:27:53 -0300

    Ex. 01 resolvido

--- questions/question_01.py
+++ questions/question_01.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# 1. Dado o tamanho da base e da altura de um retângulo, calcular a sua área e o seu perímetro.
+
+base = input("Informe a base: ")
+altura = input("Informe a altura: ")
+
+area = (base * altura)
+perimetro = (base * 2) + (altura * 2)
+
+print("\nÁrea: " + str(area))
+print("Perímetro: " + str(perimetro))
