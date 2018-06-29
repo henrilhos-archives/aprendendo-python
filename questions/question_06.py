b85344d27b90d20c1c88c5b4b8be0a2b59a4c4ae
castilh0s <henriquedecastilhos@gmail.com>
2018-06-29 19:52:01 -0300

    Ex. 06 resolvido

--- questions/question_06.py
+++ questions/question_06.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# Ler dois números inteiros e exibir o quociente e o resto da divisão inteira entre eles.
+
+num1 = input("Informe o 1º número inteiro: ")
+num2 = input("Informe o 2º número inteiro: ")
+
+quociente = int(num1) / int(num2)
+resto = int(num1) % int(num2)
+
+print("\nQuociente: " + str(quociente))
+print("Resto: " + str(resto))
