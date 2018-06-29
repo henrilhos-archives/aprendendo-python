771446aacf0b5757cc313b4178486e08e0c179c5
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2018-06-29 12:36:00 -0300

    Ex. 04 resolvido

--- questions/question_04.py
+++ questions/question_04.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# 4. Dado os três lados de um triângulo determinar o perímetro do mesmo.
+
+lado1 = input("Informe o 1º lado do triângulo: ")
+lado2 = input("Informe o 2º lado do triângulo: ")
+lado3 = input("Informe o 3º lado do triângulo: ")
+
+perimetro = lado1 + lado2 + lado3
+
+print("\nPerimetro: " + str(perimetro))
