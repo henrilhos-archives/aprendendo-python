5cede593d6c6baba2beeebddeddb0d812c1a2e55
castilh0s <henriquedecastilhos@gmail.com>
2018-06-29 19:48:35 -0300

    Ex. 05 resolvido

--- questions/question_05.py
+++ questions/question_05.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# 5. Ler um número inteiro e exibir o seu sucessor.
+
+numero = input("Informe um número inteiro: ")
+
+sucessor = int(numero) + 1
+
+print("\nSucessor: " + str(sucessor))
