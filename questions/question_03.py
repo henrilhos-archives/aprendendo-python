536d6cf2079c131e3de78e0802d6000680f83e6b
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2018-06-29 12:34:16 -0300

    Ex. 03 resolvido

--- questions/question_03.py
+++ questions/question_03.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# 3. Dado o tamanho do raio de uma circunferência, calcular a área e o perímetro da mesma.
+
+pi = 3.14
+
+raio = input("Informe o raio da circunferência: ")
+
+area = pi * (raio ** 2)
+perimetro = 2 * pi * raio
+
+print("\nÁrea: " + str(area))
+print("Perímetro: " + str(perimetro))
