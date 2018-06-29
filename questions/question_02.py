18affb5911ad3a737849375ddd19b5e0f5b74a45
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2018-06-29 12:31:53 -0300

    Ex. 02 resolvido

--- questions/question_02.py
+++ questions/question_02.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# 2. Dado o tamanho do lado de um quadrado, calcular a área e o perímetro do mesmo.
+
+lado = input("Informe a medida do lado: ")
+
+area = lado ** 2
+perimetro = lado * 4
+
+print("\nÁrea: " + str(area))
+print("Perímetro: " + str(perimetro))
