2b9f44c2250b13e92bd332d427947532e95ae4f4
castilh0s <henriquedecastilhos@gmail.com>
2018-06-29 21:11:28 -0300

    Resolvido

--- questions/question_12.py
+++ questions/question_12.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# 12. Faça um algoritmo que transforme uma velocidade fornecida em m/s pelo
+# usuário para Km/h. Para tal, multiplique o valor em m/s por 3,6.
+
+ms = input("Informe a velocidade em m/s: ")
+
+kmh = float(ms) * 3.6
+
+print("A velocidade em km/h é de: " + str(kmh))
