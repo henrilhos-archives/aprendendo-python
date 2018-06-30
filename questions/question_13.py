2049b62ea2429d90a2f4105339526ba0b381eb4c
castilh0s <henriquedecastilhos@gmail.com>
2018-06-29 21:13:44 -0300

    sei lá pô

--- questions/question_13.py
+++ questions/question_13.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# 13. Um circuito elétrico é composto de duas resistências R1 e R2 em paralelo, e
+# ambas em sequência de uma resistência R3. Faça um algoritmo para calcular a
+# resistência equivalente desse circuito.
