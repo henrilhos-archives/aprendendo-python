c8ea99f872ddc9da187d1e8f0eb0888dbefbb183
castilh0s <henriquedecastilhos@gmail.com>
2018-07-02 17:33:07 -0300

    Resolvido

--- questions/question_21.py
+++ questions/question_21.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# 21. Escreva um programa que leia um número e exiba se ele é positivo ou
+# negativo.
+
+num = input("Digite um número: ")
+
+if (int(num) > 0):
+    print("\nVocê digitou um número positivo.")
+elif (int(num) < 0):
+    print("\nVocê digitou um número negativo.")
+else:
+    print("\nVocê digitou o número zero.")
