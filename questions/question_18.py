471bfd18e900512e703827f4a1073f6c010f0177
castilh0s <henriquedecastilhos@gmail.com>
2018-07-02 17:16:19 -0300

    Resolvido

--- questions/question_18.py
+++ questions/question_18.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# 18. Escreva um programa que permute o valor de duas variáveis inteiras.
+
+a = input("Informe o valor de A: ")
+b = input("Informe o valor de B: ")
+
+aux = float(a)
+a = float(b)
+b = float(aux)
+
+print("\nValor de A: " + str(a))
+print("Valor de B: " + str(b))
