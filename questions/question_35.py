a31858dace34cac62badf741681054363e8925dd
castilh0s <henriquedecastilhos@gmail.com>
2018-07-02 16:46:11 -0300

    em progresso

--- questions/question_35.py
+++ questions/question_35.py
@@ -1,2 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# 35. Escreva um programa que leia um número inteiro de 1 a 7 e informe o dia da
+# semana correspondente, sendo domingo o dia de número 1. Se o número não
+# corresponder a um dia da semana, mostre uma mensagem de erro.
+
+ativaAviso = 0
+
+diaNumero = input("Insira o valor númerico referente ao dia da semana: ")
+
+if (int(diaNumero) == 1):
+    diaSemana = "domingo"
+elif (int(diaNumero) == 2):
+    diaSemana = "segunda-feira"
+elif (int(diaNumero) == 3):
+    diaSemana = "segunda-feira"
+elif (int(diaNumero) == 4):
+    diaSemana = "segunda-feira"
+elif (int(diaNumero) == 5):
+    diaSemana = "segunda-feira"
+elif (int(diaNumero) == 6):
+    diaSemana = "segunda-feira"
+elif (int(diaNumero) == 7):
+    diaSemana = "segunda-feira"
+else (int(diaNumero) == 2):
+    aviso = "Opção inválida!"
+    ativaAviso = 1
